78a1f06e98613dd812f48cc689c3386aa227207d
Vshishkar <vshishkarov@gmail.com>
2018-05-16 22:06:49 +0300

    update config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     'url':
-        'mongodb://localhost/photo_app'
-        //'mongodb://admin:qwertyasdf@ds115420.mlab.com:15420/photo_app'
+        //'mongodb://localhost/photo_app'
+        'mongodb://admin:admin@ds115420.mlab.com:15420/photo_app'
 };
